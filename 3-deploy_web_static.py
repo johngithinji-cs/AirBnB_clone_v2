4581c2a65bd8464514834cb66cdce4da45535afc
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-03 16:22:42 +0300

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import *
 
 env.hosts = ["34.73.5.42", " 34.73.70.108"]
 env.user = "ubuntu"
-env.key_filename = "~/.ssh/holberton"
+env.key_filename = "~/.ssh/school"
 env.warn_only = True
 
 
