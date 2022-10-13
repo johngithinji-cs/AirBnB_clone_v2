72c04b9a028887e9d8c7ce35457cb6ceaacef78c
johngithinji-cs <mathaijohn97@gmail.com>
2022-10-13 21:52:59 +0300

    adding

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """hello_route"""
+    return "Hello HBNB!”
+if __name__ == "__main__":
+    app.run()
