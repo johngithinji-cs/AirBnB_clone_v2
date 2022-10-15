f16814fd5656e8389e0522b2bcaaf490c8f3c507
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-15 08:07:14 +0300

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask"""
+"""script that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -11,8 +11,8 @@ def hello_route():
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """hbnb"""
+def hbnb_route():
+    """hbnb route"""
     return "HBNB"
 
 
