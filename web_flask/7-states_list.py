20a8e11453896846662ad0be43023cec1f9cfadd
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-16 20:44:38 +0300

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""Flask"""
+""" This module starts a Flask web application
+listening on 0.0.0.0, port 5000
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present in
+    DBStorage sorted by name (A->Z) tip
+    LI tag: description of one State: <state.id>: <B><state.name></B>
+"""
+
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route("/states_list", strict_slashes=False)
 def states_list():
-    """states_list"""
-    res = []
-    storage.reload()
-
-    for v in storage.all("State").values():
-       res.append([v.id, v.name])
-
-    return render_template("7-states_list.html", states=res)
+    """ Display a HTML page: (inside the tag BODY)
+    H1 tag: “States”
+    UL tag: with the list of all State objects present in
+    DBStorage sorted by name (A->Z) tip
+    """
+    state_list = storage.all(State).values()
+    return render_template('7-states_list.html', state_list=state_list)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """teardown"""
+def shutdown_session(exception=None):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
