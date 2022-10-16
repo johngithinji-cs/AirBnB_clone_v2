f443b2d2a699d34e214bccd3a61ab5b3d6dbf45b
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-16 20:02:38 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models import storage_type
 from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """defines class Amenity"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
