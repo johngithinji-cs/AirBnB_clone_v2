a11564862c22549377c46333ac2d5b2155630678
johngithinji-cs <mathaijohn97@gmail.com>
2022-10-16 21:40:47 +0300

    more changes

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This module starts a Flask web application
+listening on 0.0.0.0, port 5000
+Routes:
+    /hbnb_filters: display a HTML page: (inside the tag BODY)
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.city import City
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filter_hbnb_filters():
+    """
+    Display a HTML page: (inside the tag BODY)
+    """
+    state_list = storage.all(State).values()
+    return render_template("10-hbnb_filters.html", state_list=state_list)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Script to start a Flask web application. """
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_filters():
+    """ Displays a HTML page. """
+    states = storage.all('State').values()
+    cities = storage.all('City').values()
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    return render_template('10-hbnb_filters.html', **locals())
+
+
+@app.teardown_appcontext
+def closer(self):
+    """ Removes the current SQLAlchemy Session. """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
